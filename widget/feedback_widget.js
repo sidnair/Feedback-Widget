caf388e668c45134b99a897cba8978b986398985
Sid Nair <ssn2114@columbia.edu>
2011-07-23 01:54:08 -0700

    refactored to only use divs...

--- widget/feedback_widget.js
+++ widget/feedback_widget.js
@@ -1,30 +1,44 @@
 $(document).ready(function() {
 
-  var IFRAME_SOURCE = "feedback_widget.html";
+  // var IFRAME_SOURCE = "feedback_widget.html";
+  var IFRAME_CONTENT = '<!DOCTYPE html> <html>   <head>     <link href="css/ui-darkness/jquery-ui-1.8.14.custom.css" rel="stylesheet">     <link href="css/feedback_widget.css" rel="stylesheet">      <!--      <script src="js/jquery-1.5.1.min.js"></script>     <script src="js/jquery-ui-1.8.14.custom.min.js"></script>     <script src="js/application.js"></script>     -->   </head>   <body>     <div id="container">       <div id="title" class="section">         <div id="hideButton">&ndash;</div>         <h1>Send Feedback</h1>       </div>     <div class="section">       <form>         <div id="controls">           <input type="radio" id="highlight" name="radio" />           <label for="highlight">Highlight</label>                      <input type="radio" id="blackout" name="radio" checked="checked" />           <label for="blackout">Blackout</label>                     <input type="radio" id="annotate" name="radio" />           <label for="annotate">Annotate</label>         </div>       </form>     </div>       <div id="description" class="section">         <span>Please describe the problem:</span>         <div id="textAreaWrapper">           <textarea></textarea>         </div>       </div>       <div id="submitControls" class="section">         <span class="button" id="submit">           <button>Submit</button>         </span>       </div>     </div>   </body> </html>'
   var IFRAME_OVERLAY_SOURCE = "overlay_page.html";
   var BASE_Z_INDEX = 10000;
 
+  function getFrameStyle(styleObj) {
+    var styleAttrsString = '';
+    $.each(styleObj, function(k, v) {
+       styleAttrsString += k + ':' + v + ';';
+    });
+    return styleAttrsString;
+  }
+
   function injectWidgetLayout() {
-    styleAttrs = {
+    var styleAttrsString = getFrameStyle({
       width: '275px',
-      height: '275px',
-      bottom: '0',
+      height: '275px', bottom: '0',
       right: '5px',
       position: 'fixed',
       outline: '0',
-      border: '1px solid black',
-      "z-index": BASE_Z_INDEX + 1,
-      background: 'white'
-    };
-    var styleAttrsString = ''
-    $.each(styleAttrs, function(k, v) {
-       styleAttrsString += k + ':' + v + ';';
+      'z-index': BASE_Z_INDEX + 1,
+      background: 'white',
+      border: 'none',
     });
+    $('<div id="widgetFrame">' + IFRAME_CONTENT + '</div>')
+        .appendTo($('body'));
+    $('#controls').buttonset();
+    $('button').button();
+    $('#hideButton').hover(function() {
+      $(this).css({
+      });
+    });
+    /*
     $('<iframe />', {
       id: 'widgetFrame',
       src: IFRAME_SOURCE,
       style: styleAttrsString
     }).appendTo($('body'));
+    */
   }
 
   /*
@@ -53,6 +67,20 @@ $(document).ready(function() {
   */
 
   function injectOverlayLayout() {
+    // for now, just reduce opacity...
+    var OPACITY_MULTIPLIER = 0.6;
+    function setOpacity(el, n) {
+      $(el).css('opacity', n)
+    }
+
+    function getOpacity(el) {
+      return $(el).css('opacity');
+    }
+
+    $('*', document.body).each(function(i, el) {
+      setOpacity(el, getOpacity(el) * OPACITY_MULTIPLIER);
+    });
+    /*
     var pane = $('<div class="overlay" id="pane"></div>');
     pane.appendTo($('body'));
     pane.css({
@@ -65,8 +93,72 @@ $(document).ready(function() {
       zIndex: '10000',
       opacity: 0.55
     });
+    */
   }
 
-  injectWidgetLayout();
+  /*
+  function injectInvisibleHtml() {
+    var iframe = $('<iframe id="invisibleFrame"></iframe>');
+    $('*').each(function(i, el) {
+      var name = el.nodeName;
+      if (name !== 'SCRIPT' && name !== 'LINK') {
+        var newNode = $(document.createElement(name));
+      }
+    });
+  }
+  */
+
+  var overlayElements = {
+  };
+
+  var highlight, unHighlight, blackout, unBlackout;
+
+  function highlight() {
+    $('<div></div>')
+        .offset($(this).offset())
+        .width($(this).widht())
+        .height($(this).height());
+  }
+
+  function blackout(e, el) {
+    el.addClass('focus');
+  }
+
+  function unBlackout() {
+  }
+
+  var markupTools = {
+    highlight: [highlight, unHighlight],
+    blackout: [blackout, unBlackout]
+  };
+
+  var activeTool = 'blackout';
+
+  // TODO - maybe run some filtering here
+  function runActiveMarkup(e, el) {
+    markupTools[activeTool][0](e, el);
+  }
+
+  function runActiveUndoMarkup(e) {
+    markupTools[activeTool][1](e, el);
+  }
+
+  function registerListeners() {
+    var highlightableElements = ['h1'];
+    var orSelector = highlightableElements.join(',');
+    $(orSelector).hover(
+      /* mouse over */ function(e) { runActiveMarkup(e, $(this)) }, 
+      /* mouse out */ function(e) { runActiveUndoMarkup(e, $(this)) });
+
+    //TODO: add click listeners
+
+    // $.each(highlightableElements, function(i, name) { });
+  }
+
+
+  // injectInvisibleHtml();
   injectOverlayLayout();
+  injectWidgetLayout();
+  registerListeners();
+
 });
--- widget/js/application.js
+++ widget/js/application.js
@@ -6,38 +6,3 @@ $(function() {
     });
   });
 });
-
-
-
-
-var overlayElements = {
-};
-
-var highlight, unHighlight, blackout, unBlackout;
-
-var markupTools = {
-  highlight: [highlight, unHighlight],
-  blackout: [blackout, unBlackout]
-};
-
-// TODO - maybe run some filtering here
-function runActiveMarkup(e) {
-  markupTools[activeTool][0](e);
-}
-
-function runActiveUndoMarkup(e) {
-  markupTools[activeTool][1](e);
-}
-
-function registerListeners() {
-  var highlightableElements = [];
-  var orSelector = highlightableElements.join(',');
-  $(orSelector).hover(
-    /* mouse over */ function(e) { runActiveMarkup(e) }, 
-    /* mouse out */ function(e) { runActiveUndoMarkup(e) });
-
-  //TODO: add click listeners
-
-  // $.each(highlightableElements, function(i, name) { });
-}
-
