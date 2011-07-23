c7087d8ee9d776d861c8b2f594c8592d9adbd212
Sid Nair <ssn2114@columbia.edu>
2011-07-22 20:04:08 -0700

    initialized repo and default rails project

---
+++ feedback_backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackBackend::Application.config.secret_token = '6545651db3031ac397f0b01698321de460c37938b1b35fc68ce398659cf4fc6ae4ff5c2c4184b06b5c0b7b555c752044bd227d578be0790e88dd4e80edca1c2f'
