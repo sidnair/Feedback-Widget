85ff4892d4fdc554d2aa7811d7fc603174b33e64
Sid Nair <ssn2114@columbia.edu>
2011-07-23 00:06:23 -0700

    added overlay

--- widget/feedback_widget.js
+++ widget/feedback_widget.js
@@ -1,29 +1,72 @@
 $(document).ready(function() {
 
   var IFRAME_SOURCE = "feedback_widget.html";
+  var IFRAME_OVERLAY_SOURCE = "overlay_page.html";
+  var BASE_Z_INDEX = 10000;
 
-  function injectLayout() {
+  function injectWidgetLayout() {
     styleAttrs = {
       width: '275px',
-      height: '265px',
+      height: '275px',
       bottom: '0',
       right: '5px',
       position: 'fixed',
       outline: '0',
-      border: '1px solid black'
+      border: '1px solid black',
+      "z-index": BASE_Z_INDEX + 1,
+      background: 'white'
     };
     var styleAttrsString = ''
     $.each(styleAttrs, function(k, v) {
        styleAttrsString += k + ':' + v + ';';
     });
     $('<iframe />', {
+      id: 'widgetFrame',
       src: IFRAME_SOURCE,
-      "z-index": '10004',
       style: styleAttrsString
     }).appendTo($('body'));
-
   }
 
-  injectLayout();
+  /*
+  function injectOverlayLayout() {
+    styleAttrs = {
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      background: '#666',
+      outline: '0',
+      opacity: 0.4,
+      "z-index": BASE_Z_INDEX
+    };
+    var styleAttrsString = ''
+    $.each(styleAttrs, function(k, v) {
+       styleAttrsString += k + ':' + v + ';';
+    });
+    $('<iframe />', {
+      id: 'overlayFrame',
+      src: IFRAME_OVERLAY_SOURCE,
+      style: styleAttrsString,
+    }).appendTo($('body'));
+  }
+  */
 
+  function injectOverlayLayout() {
+    var pane = $('<div class="overlay" id="pane"></div>');
+    pane.appendTo($('body'));
+    pane.css({
+      position: 'fixed',
+      top: '0%',
+      left: '0%',
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'black',
+      zIndex: '10000',
+      opacity: 0.55
+    });
+  }
+
+  injectWidgetLayout();
+  injectOverlayLayout();
 });
--- widget/installation.js
+++ widget/installation.js
@@ -3,7 +3,7 @@
   var firstScriptElement = document.getElementsByTagName('script')[0] ||
       document.body.firstChild;
 
-  function addElement(src) {
+  function addScript(src) {
     var newScriptElement = document.createElement('script');
     newScriptElement.async = true;
     newScriptElement.src =
@@ -13,7 +13,17 @@
         firstScriptElement);
   }
 
-  addElement('ajax.googleapis.com/ajax/libs/jquery/1/jquery.js');
-  addElement('columbia.edu/~ssn2114/scripts/feedback_widget.js');
+  function addCSS(src) {
+    var newElement = document.createElement('link');
+    newScriptElement.href =
+        ('https:' === document.location.protocol ? 'https://' : 'http://')
+        + src;
+    firstScriptElement.parentNode.insertBefore(newScriptElement,
+        firstScriptElement);
+  }
+
+  addScript('ajax.googleapis.com/ajax/libs/jquery/1/jquery.js');
+  addScript('columbia.edu/~ssn2114/scripts/feedback_widget.js');
+  addScript('columbia.edu/~ssn2114/scripts/injected_css.css');
 
 })();
